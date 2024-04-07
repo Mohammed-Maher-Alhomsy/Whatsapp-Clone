4e004adbc384b1cdde73efc6853967e969b5d713
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-04-07 04:12:59 +0300

    install clerk and expo-secure-store packages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,35 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+import * as SecureStore from "expo-secure-store";
 
 export { ErrorBoundary } from "expo-router";
 
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return;
+    }
+  },
+};
+
+const InitialLayout = () => {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
@@ -28,27 +51,34 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
+  return (
+    <Stack screenOptions={{ statusBarStyle: "dark" }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+
+      <Stack.Screen
+        name="otp"
+        options={{ headerTitle: "Enter Your Phone Number" }}
+      />
+
+      <Stack.Screen
+        name="verify/[phone]"
+        options={{
+          headerBackTitle: "Edit number",
+        }}
+      />
+    </Stack>
+  );
+};
 
 function RootLayoutNav() {
   return (
-    <>
-      <Stack screenOptions={{ statusBarStyle: "dark" }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-
-        <Stack.Screen
-          name="otp"
-          options={{ headerTitle: "Enter Your Phone Number" }}
-        />
-
-        <Stack.Screen
-          name="verify/[phone]"
-          options={{
-            headerBackTitle: "Edit number",
-          }}
-        />
-      </Stack>
-    </>
+    <ClerkProvider
+      publishableKey={clerkPublishableKey!}
+      tokenCache={tokenCache}
+    >
+      <InitialLayout />
+    </ClerkProvider>
   );
 }
+
+export default RootLayoutNav;
