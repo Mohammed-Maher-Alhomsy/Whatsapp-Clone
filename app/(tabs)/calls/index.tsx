0558ec0b5ad0f92db7d3decc4a512907ed525d88
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-04-07 22:42:47 +0300

    install date-fns package, add some dummy data and show the calls data inside calls screen

--- app/(tabs)/calls/index.tsx
+++ app/(tabs)/calls/index.tsx
@@ -1,11 +1,25 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { useState } from "react";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
 import { Stack } from "expo-router";
 import { TouchableOpacity } from "react-native-gesture-handler";
+
 import Colors from "@/constants/Colors";
+import calls from "@/assets/data/calls.json";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
+import { format } from "date-fns";
 
 const Page = () => {
   const [isEditing, setIsEditing] = useState(false);
+  const [items, setItems] = useState(calls);
 
   const onEdit = () => {
     setIsEditing((prev) => !prev);
@@ -26,11 +40,69 @@ const Page = () => {
         }}
       />
 
-      <ScrollView contentInsetAdjustmentBehavior="automatic"></ScrollView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={defaultStyles.block}>
+          <FlatList
+            data={items}
+            scrollEnabled={false}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => (
+              <View style={defaultStyles.separator} />
+            )}
+            renderItem={({ item }) => (
+              <View style={[defaultStyles.item]}>
+                <Image style={styles.avatar} src={item.img} />
+
+                <View style={{ flex: 1, gap: 2 }}>
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      color: item.missed ? Colors.red : "#000",
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+
+                  <View style={{ flexDirection: "row", gap: 4 }}>
+                    <Ionicons
+                      size={16}
+                      color={Colors.gray}
+                      name={item.video ? "videocam" : "call"}
+                    />
+
+                    <Text style={{ color: Colors.gray, flex: 1 }}>
+                      {item.incoming ? "Incoming" : "Outgoing"}
+                    </Text>
+                  </View>
+                </View>
+
+                <View
+                  style={{ flexDirection: "row", gap: 6, alignItems: "center" }}
+                >
+                  <Text style={{ color: Colors.gray }}>
+                    {format(item.date, "MM.dd.yy")}
+                  </Text>
+                  <Ionicons
+                    size={24}
+                    color={Colors.primary}
+                    name="information-circle-outline"
+                  />
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+});
