c84beca4f04ccaf6855c498434a2ea82fd42a865
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-04-07 19:57:44 +0300

    add some basic styles to tabs screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet } from "react-native";
+
 import { Tabs } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  Ionicons,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Colors from "@/constants/Colors";
 
 const Layout = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: Colors.background,
+          },
+          tabBarActiveTintColor: Colors.primary,
+          tabBarInactiveBackgroundColor: Colors.background,
+          tabBarActiveBackgroundColor: Colors.background,
+          headerStyle: { backgroundColor: Colors.background },
+          headerShadowVisible: false,
+        }}
+      >
         <Tabs.Screen
-          name="chats"
+          name="updates"
           options={{
+            title: "Updates",
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="chatbox" color={color} size={size} />
+              <MaterialIcons name="update" color={color} size={size} />
             ),
           }}
         />
@@ -20,26 +37,13 @@ const Layout = () => {
         <Tabs.Screen
           name="calls"
           options={{
+            title: "Calls",
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="call" color={color} size={size} />
-            ),
-          }}
-        />
-
-        <Tabs.Screen
-          name="updates"
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="cloud-upload" color={color} size={size} />
-            ),
-          }}
-        />
-
-        <Tabs.Screen
-          name="settings"
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="settings" color={color} size={size} />
+              <MaterialCommunityIcons
+                name="phone-outline"
+                color={color}
+                size={size}
+              />
             ),
           }}
         />
@@ -47,8 +51,29 @@ const Layout = () => {
         <Tabs.Screen
           name="communities"
           options={{
+            title: "Communities",
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="people" color={color} size={size} />
+              <MaterialIcons name="people" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="chats"
+          options={{
+            title: "Chats",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="chatbubbles" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="cog" color={color} size={size} />
             ),
           }}
         />
